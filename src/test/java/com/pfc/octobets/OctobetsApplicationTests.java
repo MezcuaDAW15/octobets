0e6def408ce622c4c0362b34af2b9570e0e6fab0
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-04-29 19:26:58 +0200

    Initial commit

---
+++ src/test/java/com/pfc/octobets/OctobetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc.octobets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctobetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
