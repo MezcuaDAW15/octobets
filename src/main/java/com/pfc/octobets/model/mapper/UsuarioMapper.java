ee37c776dc9ac22f0f02e16d55b7bb57d0349fc7
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-05-06 15:33:16 +0200

    Agregar mapeo para ignorar passwordHash en UsuarioMapper

--- src/main/java/com/pfc/octobets/model/mapper/UsuarioMapper.java
+++ src/main/java/com/pfc/octobets/model/mapper/UsuarioMapper.java
@@ -1,6 +1,7 @@
 package com.pfc.octobets.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.pfc.octobets.model.dto.UsuarioDTO;
 import com.pfc.octobets.repository.entity.Usuario;
@@ -9,5 +10,6 @@ import com.pfc.octobets.repository.entity.Usuario;
 public interface UsuarioMapper {
     UsuarioDTO toDTO(Usuario entity);
 
+    @Mapping(target = "passwordHash", ignore = true)
     Usuario toEntity(UsuarioDTO dto);
 }
