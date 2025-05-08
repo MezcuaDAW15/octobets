4bf00b25135b2cf15377a73e70284e556099e3c1
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-05-08 21:00:14 +0200

    Agregar OpcionRestController, OpcionService y OpcionServiceImpl para manejar opciones de apuestas

--- src/main/java/com/pfc/octobets/repository/dao/OpcionRepository.java
+++ src/main/java/com/pfc/octobets/repository/dao/OpcionRepository.java
@@ -1,9 +1,13 @@
 package com.pfc.octobets.repository.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pfc.octobets.repository.entity.Opcion;
 
 public interface OpcionRepository extends JpaRepository<Opcion, Long> {
+
+    List<Opcion> findAllByApuestaId(Long idApuesta);
 
 }
---
+++ src/main/java/com/pfc/octobets/service/OpcionServiceImpl.java
@@ -0,0 +1,46 @@
+package com.pfc.octobets.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pfc.octobets.model.dto.OpcionDTO;
+import com.pfc.octobets.model.mapper.OpcionMapper;
+import com.pfc.octobets.repository.dao.OpcionRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class OpcionServiceImpl implements OpcionService {
+
+    @Autowired
+    private OpcionRepository opcionRepository;
+
+    @Autowired
+    private OpcionMapper opcionMapper;
+
+    @Override
+    public List<OpcionDTO> findAllByApuestaId(Long idApuesta) {
+        log.info("Búsqueda de todas las opciones de la apuesta con id={}", idApuesta);
+        return opcionRepository.findAllByApuestaId(idApuesta).stream()
+                .map(opcionMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public OpcionDTO findById(Long idApuesta, Long idOpcion) {
+        log.info("Búsqueda de opción con id={} de la apuesta con id={}", idOpcion, idApuesta);
+        return opcionRepository.findById(idOpcion)
+                .filter(opcion -> opcion.getApuesta().getId().equals(idApuesta))
+                .map(opcionMapper::toDTO)
+                .orElseThrow(() -> {
+                    String msg = "Opción no encontrada con id=" + idOpcion + " de la apuesta con id=" + idApuesta;
+                    log.warn(msg);
+                    return new ResourceNotFoundException(msg);
+                });
+    }
+
+}
