f2c0f633d04e0e62e805e5bb66562d2c7127b4f3
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-05-08 20:37:57 +0200

    Actualizar mapeo en CarteraMapper, OpcionMapper y TransaccionMapper para usar nombres de métodos consistentes y agregar configuraciones de mapeo

--- src/main/java/com/pfc/octobets/model/mapper/OpcionMapper.java
+++ src/main/java/com/pfc/octobets/model/mapper/OpcionMapper.java
@@ -1,13 +1,17 @@
 package com.pfc.octobets.model.mapper;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.pfc.octobets.model.dto.OpcionDTO;
 import com.pfc.octobets.repository.entity.Opcion;
 
 @Mapper(componentModel = "spring", uses = { ApuestaMapper.class })
 public interface OpcionMapper {
-    OpcionDTO toDTO(Opcion entity);
+    @Mapping(source = "apuesta", target = "apuestaDTO")
+    OpcionDTO toDto(Opcion opcion);
 
+    @InheritInverseConfiguration
     Opcion toEntity(OpcionDTO dto);
 }
