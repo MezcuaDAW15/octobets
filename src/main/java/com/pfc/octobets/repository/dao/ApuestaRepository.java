7b0256e826b7de54960d765f0e1425697c4d2e26
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-05-18 13:17:17 +0200

    Agregar método findAll en ApuestaRepository para cargar opciones asociadas

--- src/main/java/com/pfc/octobets/repository/dao/ApuestaRepository.java
+++ src/main/java/com/pfc/octobets/repository/dao/ApuestaRepository.java
@@ -1,5 +1,8 @@
 package com.pfc.octobets.repository.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import com.pfc.octobets.repository.entity.Apuesta;
 @Repository
 public interface ApuestaRepository extends JpaRepository<Apuesta, Long> {
 
+    @EntityGraph(attributePaths = "opciones")
+    List<Apuesta> findAll();
 }
