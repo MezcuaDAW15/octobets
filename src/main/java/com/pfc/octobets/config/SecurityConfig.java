1c9da448f4fc08c25acb2623be103a662e6c4760
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-05-20 22:12:45 +0200

    Agregar configuración de seguridad para saltar el login por defecto

--- src/main/java/com/pfc/octobets/config/SecurityConfig.java
+++ src/main/java/com/pfc/octobets/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.pfc.octobets.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // deshabilita CSRF si no usas formularios web
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // permite todas las rutas
+                )
+                .httpBasic().disable() // deshabilita Basic Auth
+                .formLogin().disable(); // deshabilita form-login
+
+        return http.build();
     }
 }
